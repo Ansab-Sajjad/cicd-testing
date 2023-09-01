48ff1c47242c09d81a2fb28983867609d926df8c
Syed Ansab Sajjad <115165376+Ansab-Sajjad@users.noreply.github.com>
2023-09-01 17:12:03 +0500

    Create important_code.cy.js

---
+++ cypress/e2e/important_code.cy.js
@@ -0,0 +1,2 @@
+    cy.get('.custom-select__option:first').click().wait(100)  //====> To Select first option from any dropdown
+    cy.get('.custom-select__option:eq(0)').click().wait(100) //====> array base.
